ee7a06fe461c6fe797c4d78627511bcc1cfff09f
Phil O'Grady <>
2023-10-02 15:17:14 +0100

    improve card layout

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import {Avatar, Grid, Paper, styled} from "@mui/material";
 import Typography from "@mui/material/Typography";
+import {deepOrange} from "@mui/material/colors";
 
 
-const Item = styled(Paper)(({ theme }) => ({
+const Item = styled(Paper)(({theme}) => ({
     ...theme.typography.body2,
     textAlign: 'center',
     color: theme.palette.text.secondary,
@@ -16,20 +17,23 @@ function About() {
     return (
         <div className="About">
             {['Katie', 'Leanne'].map((name) => (
-                <Item key={name} elevation={8} style={{margin: '24px', padding: '24px'}}>
-                    <Grid container spacing={2}>
+                <Item key={name} elevation={8} style={{margin: '24px'}}>
+                    <Grid container spacing={2} style={{height: '100%'}}>
                         <Grid xs={2}>
-                            <Avatar alt="Cindy Baker" src="/static/images/avatar/3.jpg" />
+                            <Avatar src="/broken-image.jpg" sx={{width: 56, height: 56}}/>
                         </Grid>
                         <Grid xs={2}>
                             {name}
                         </Grid>
                         <Grid xs={6}>
-                        <Typography>Some bio content</Typography>
+                            <Typography>Lorem ipsum dolor sit amet. At blanditiis culpa quo consequuntur quos est quae
+                                enim et perferendis ratione. In expedita exercitationem aut aliquam temporibus qui modi
+                                sunt ab eius corporis et sint molestias est iure iure. Et rerum aspernatur et quaerat
+                                dolorem qui numquam nulla et suscipit facere aut tempora officia? Aut tempora
+                                perspiciatis cum harum odit eum repellendus iste qui dicta nulla et ullam doloremque et
+                                iste distinctio ut quia quaerat.</Typography>
                         </Grid>
                     </Grid>
-
-
 
 
                 </Item>
